a6d41b2f2dd9343e09fe0421c4b186513c81cfb0
KavvaP17 <KavvaP17@list.ru>
2017-03-25 13:26:08 +0300

    additional_3/feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function getBase(base) {
-  // your solution
+module.exports = function getBase(num) {
+  let tmp=num.split('').reduce((prev,next)=>Math.max(prev,next));
+  if (tmp===1 && num.split('').length>1){
+    return '9'.repeat(num.split('').length-1);
+  }
+  let base=2;
+  while (true){
+    let numOne=(Math.log(num)/Math.log(base)|0)+1;
+    if (parseInt('1'.repeat(numOne),base)===(+num)){
+      return base;
+    }
+    base++;
+  }
 }
